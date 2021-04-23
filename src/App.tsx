fdd24b57ac1c459e32321fc116dd70363541a0aa
yiqgao <yiqgao@outlook.com>
2021-04-23 14:41:21 +0800

    initial this project

---
+++ src/App.tsx
@@ -0,0 +1,15 @@
+import React from "react";
+import { BrowserRouter as Router, Switch, Route, Link } from "react-router-dom";
+
+export interface Props {}
+class App extends React.Component<Props> {
+    constructor(props: Props) {
+        super(props);
+    }
+    render() {
+        return (
+            <div>demo</div>
+        );
+    }
+}
+export default App;
